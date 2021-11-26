1721dd465ec23f3ab4a694162a95d92ab245dc9e
liars0000 <mildmild2703@gmail.com>
2021-11-26 18:45:06 +0700

    new file

---
+++ DSProject-iCase/pages/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def home_view(request,*args,**kwargs):
+    return render(request,"main.html",{})
+
+def cases_view(request,*args,**kwargs):
+    return render(request,"cases.html",{})
